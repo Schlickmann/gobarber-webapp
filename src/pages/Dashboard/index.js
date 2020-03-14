7ae16c5683da774718e847ab6409a0a5c376089c
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-13 19:37:29 -0700

    added handle next day and previous day dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
-
-import { authContext } from '~/contexts/AuthContext';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+  const dateFormatted = useMemo(() => format(date, 'MMMM do'), [date]);
+
+  function handlePreviousDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePreviousDay}>
           <MdChevronLeft size={36} color="#fe346e" />
         </button>
-        <strong>March 13th</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#fe346e" />
         </button>
       </header>
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,5 +1,4 @@
 import styled, { css } from 'styled-components';
-import { lighten } from 'polished';
 
 export const Container = styled.div`
   max-width: 600px;
